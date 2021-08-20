1195dfd14fbb1c793b8d17f55fb98ad528384b8c
A. Rzhanitsyn <risdeep@gmail.com>
2021-08-20 23:55:21 +0500

    addd

--- A/index.js
+++ A/index.js
@@ -1,26 +1,44 @@
 const findLatestWeight = (weights) => {
   // let current = weights[0];
+  weights.sort((a, b) => a - b); // 1,1,2,4,7,8
 
   while (weights.length > 1) {
-    // current = weights[0];
-    let max1 = -Infinity
-    let max2 = -Infinity
+    console.log({ weights });
+    const weights2 = [];
+    const max1 = weights.pop();
+    const max2 = weights.pop();
 
-    for (let index = 0; index < weights.length; index++) {
-      if (max1 > max2) {
-        
-      }
-      if (weights[index] === weights[index - 1]) {
-        weights[index - 1] = 0;
-      } else {
-        weights[index - 1] = weights[index] - weights[index - 1];
-      }
+    const diff = max1 - max2;
+    console.log({ max1, max2, diff });
+    let isPivotFound = false;
 
-      index -= 1;
+    if (diff !== 0) {
+      for (let index = 0; index < weights.length; index++) {
+        const current = weights[index];
+
+        if (!isPivotFound && diff <= current) {
+          weights2.push(diff);
+          isPivotFound = true;
+        }
+        weights2.push(current);
+
+        console.log(weights2);
+        // index -= 1;
+      }
+      weights = weights2;
     }
   }
 
-  return weights[0];
+  return weights[0] || 0;
 };
-
+findLatestWeight([2, 7, 4, 1, 8, 1]);
 module.exports = findLatestWeight;
+/*
+[2,7,4,1,8,1]
+8,7,4,2,1,1
+
+[2,4,1,1,1]
+[2,1,1,1]
+[1,1,1]
+[1]
+*/
--- A/index.test.js
+++ A/index.test.js
@@ -2,6 +2,8 @@ const inputProcessing = require('./index');
 
 const testData = [
   [[2, 7, 4, 1, 8, 1], 1], //
+  [[2, 2], 0], //
+  [[2], 2], //
 ];
 
 testData.forEach(([input, output], index) => {
